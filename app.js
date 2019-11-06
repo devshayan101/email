ffd850b3b39940a80b0f54e8f89e1e730f48ae8c
Dev <shayan.dev98@gmail.com>
2019-11-06 15:16:16 +0530

    eight

--- app.js
+++ app.js
@@ -39,16 +39,16 @@ app.post('/send', (req, res) => {
   async function main() {
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
+    //let testAccount = await nodemailer.createTestAccount();
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
+      host: 'smtp.gmail.com',
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass // generated ethereal password
+        user: process.env.USER, // generated ethereal user
+        pass: process.env.PASS // generated ethereal password
       }
     });
 
